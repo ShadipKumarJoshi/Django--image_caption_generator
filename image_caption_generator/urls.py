24239c4bf0ff1852547f5d1a2c12dbab384845c9
Shadip Kumar Joshi <220049@softwarica.edu.np>
2025-06-28 09:19:34 +0545

    update

--- image_caption_generator/urls.py
+++ image_caption_generator/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from captions import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('captions.urls')),
+    path('', views.upload_image, name='upload_image'),
+    path('captions/', include('captions.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
